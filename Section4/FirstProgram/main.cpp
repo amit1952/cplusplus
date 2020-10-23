30b5e1a2ae636d6cd01e06e34a278d13c1beee11
Amitava Das Gupta <amitavadg@gmail.com>
2020-10-23 10:10:39 +0530

    Changes program to display the number entered.

--- Section4/FirstProgram/main.cpp
+++ Section4/FirstProgram/main.cpp
@@ -4,6 +4,6 @@ int main() {
     int favourite_number;
     std::cout << "Enter your favourite number between 1 and 100: ";
     std::cin >> favourite_number;
-    std::cout << "Amazing! That's my favourite number too!" << std::endl;
+    std::cout << "Amazing!! " << favourite_number << " is my favourite number too!" << std::endl;
     return 0;
 }
